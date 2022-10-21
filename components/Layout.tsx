6add4235cb3923dddfcfe48fa022272caf6d0332
yusuke omi <ysk.omi0818@gmail.com>
2022-10-21 13:09:35 +0900

    [update] ベースのCSSを追加

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,11 @@
 import { PropsWithChildren } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import styles from '../styles/Layout.module.scss';
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <Header></Header>
       <main>{children}</main>
       <Footer></Footer>
