da9242b70271ad8095590c23ac37e64ee3d428ce
yusuke omi <ysk.omi0818@gmail.com>
2022-10-21 14:23:54 +0900

    [update] CheckboxのCSSを追加

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -1,8 +1,15 @@
+import styles from '../styles/Checkbox.module.scss';
+
 export default function Checkbox({ prefacture, checked, onChange }) {
   return (
-    <label>
-      <input type='checkbox' checked={checked} onChange={() => onChange(prefacture.prefCode)} />
-      <span>{prefacture.prefName}</span>
+    <label className={styles.label}>
+      <input
+        className={styles.input}
+        type='checkbox'
+        checked={checked}
+        onChange={() => onChange(prefacture.prefCode)}
+      />
+      <span className={styles.text}>{prefacture.prefName}</span>
     </label>
   );
 }
