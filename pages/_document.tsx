525a0b4e0fa9a151dfe93908c7d265729432f4ca
yusuke omi <ysk.omi0818@gmail.com>
2022-10-21 16:59:54 +0900

    [update] crossoriginの部分をReactのルールに従って書き換え

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ const MyDocument = () => {
     <Html lang='ja'>
       <Head>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
         <link href='https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap' rel='stylesheet' />
       </Head>
       <body>
