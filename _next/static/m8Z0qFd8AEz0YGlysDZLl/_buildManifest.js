2d6d62ab797dec952874f77f40c929b03d7ce172
ysk-omi <ysk-omi@users.noreply.github.com>
2022-10-26 04:12:13 +0000

    deploy: f6f1d219cdd7c1abd3d06a5da4069eab3a76ab69

---
+++ _next/static/m8Z0qFd8AEz0YGlysDZLl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/4b358913-7472cce777401008.js","static/css/c658b4d313021ec9.css","static/chunks/pages/index-c6f61bb8e28c2874.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
